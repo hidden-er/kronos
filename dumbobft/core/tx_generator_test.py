4e94e60c4a2c13dfe25a8f091817f71ae0459c89
AndersenLi108 <2350635839@qq.com>
2023-12-26 06:17:37 -0800

    remove useless files

--- dumbobft/core/tx_generator_test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import time
-
-#s=pickle.load(open('/home/lyn/BDT/TXs','rb'))
-s=pickle.load(open('/home/lyn/BDT/TXs_file/TXs0','rb'))
-print(len(s))
-for i in s:
-    print(i)
-    time.sleep(2)
