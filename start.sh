45bea2610e2fc2346db0aca3d99ec384847619e4
AndersenLi108 <129189624+AndersenLi108@users.noreply.github.com>
2024-01-07 21:10:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "start.sh <shard_id> <N> <f> <B> <K> <R> <start_num> <num> <tx_num>"
+echo "start.sh <shard_num> <N> <f> <B> <R> <start_num> <num> <tx_num>"
 
 # ./start.sh 3 4 1 2000 1 0 8 20000
 
 rm ./TXs
 touch TXs
-python3 ./dumbobft/core/tx_generator.py --shard_num $1 --tx_num 20000
+python3 ./dumbobft/core/tx_generator.py --shard_num $1 --tx_num $8
 #python3 run_trusted_key_gen.py --N $2 --f $3
 
 
@@ -19,9 +19,9 @@ while [ "$shard_id" -lt $1 ]; do
         if [ $(( $shard_id * $2 + $node_id )) -ge $6 ] && [ $(( $shard_id * $2 + $node_id )) -lt $(($6 + $7)) ]; then
           echo "start node $node_id in shard $shard_id..."
           python3 run_socket_node.py --sid 'sidA' --id $node_id --shard_id $shard_id --shard_num $1 --N $2 --f $3 --B $4 --R $5 --tx_num $8 &
-          rm "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
+          #rm "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
           rm "./log/consensus-node-$(($shard_id * $2 + $node_id)).log"
-          cp ./TXs "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
+          #cp ./TXs "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
         else
           echo "do nothing"
         fi
