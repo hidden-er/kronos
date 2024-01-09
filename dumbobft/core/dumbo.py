1d2afda5dec9fa73c32080f2606717946a9703ba
AndersenLi108 <129189624+AndersenLi108@users.noreply.github.com>
2024-01-09 15:59:06 +0800

    modify the method to send LD message

--- dumbobft/core/dumbo.py
+++ dumbobft/core/dumbo.py
@@ -321,15 +321,15 @@ class Dumbo():
                 if receive:
                     # receive LD message from other shard
                     #print("[msg]:", type(msg), msg)
-                    tx_batch, proof, rt, shard_branch, positions = msg
+                    txs, proof, rt, shard_branch, positions = msg
                     '''print('[LD] Node %d in shard %d receive LD message from %d' % (self.id, self.shard_id,sender))'''
                     #print(tx_batch)        
                     # extract the transactions output shard of which is this shard
-                    txs = [tx for tx in json.loads(tx_batch) if parse_shard_info(tx)[2] == self.shard_id]
+                    #txs = [tx for tx in json.loads(tx_batch) if parse_shard_info(tx)[2] == self.shard_id]
 
                     #if do not exist tx that output shard == self.shard_id
-                    if len(txs) == 0:
-                        break
+                    # if len(txs) == 0:
+                    #     break
 
                     # construct merkle tree of these transactions
                     val = merkleTree(txs)[1]
@@ -662,10 +662,19 @@ class Dumbo():
 
         # broadcast LD message except itself
         # (TODO: Sent according to the shards involved)
-        if self.id == 0:
-                send(-3, ('LD', '', (txs, Sigma, rt, shard_branch, positions)))
+        # if self.id == 0:
+        #         send(-3, ('LD', '', (txs, Sigma, rt, shard_branch, positions)))
                 #print("shard ", self.shard_id, " round ", self.epoch, " send LD message to other shards")
-        #print("shard %d node %d gets return values" %(self.shard_id, self.id))
+        grouped_txs = defaultdict(list)
+        txs_list = json.loads(txs)
+        for tx in txs_list:
+            _, _, output_shard, _ = parse_shard_info(tx)
+            grouped_txs[output_shard].append(tx)
+        
+        if self.id == 0:
+            for shard_id in grouped_txs:
+                for i in range(N):
+                    send(i + shard_id * N, ('LD', '', (grouped_txs[shard_id], Sigma, rt, shard_branch, positions)))
 
 
         if self.id == 1:
