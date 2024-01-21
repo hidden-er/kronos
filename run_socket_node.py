37cc30a817352e3292a799d431a205b29204f3da
hiddener <hiddeneer@gmail.com>
2024-01-21 11:42:58 +0800

    Update run_socket_node.py

--- run_socket_node.py
+++ run_socket_node.py
@@ -197,11 +197,11 @@ if __name__ == '__main__':
         bft_thread.start()
         bft_thread.join()
 
-    time.sleep(20)
+    time.sleep(60)
     with stop.get_lock():
         stop.value = True
         #print("shard_id ", shard_id, "node ",i," stop; total time:",time.time()-start - 2)
-        total_time = time.time()-start - 20
+        total_time = time.time()-start - 60
 
 
     with open(f'log/consensus-node-{i + shard_id * N}.log','r') as f:
